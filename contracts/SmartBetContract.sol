cbb5406da8c8899ffefbbf04152c45aba1d0d688
ulyh <janeuly@pm.me>
2024-03-04 14:13:18 +0100

    the 5 best winners

--- contracts/SmartBetContract.sol
+++ contracts/SmartBetContract.sol
@@ -3,56 +3,95 @@ pragma solidity ^0.8.24;
 
 contract SmartBetContract {
     address private admin;
-    uint increment;
+
     struct Player {
         uint id;
+        address player_address;
         string pseudo;
-
+        uint256 victories;
     }
-    mapping (address => Player) public players;
-    event NewPlayer(uint256 id, string pseudo);
-    event WinTheBet(Player winner, uint256 matchId);
-
     struct Match {
         uint256 teamA;
         uint256 teamB;
     }
 
+    Player[] players;
+    uint256 participant_number;
     Match[] private matchs;
 
     // map of array of Players who won the bet by matchId
-    mapping (uint256 => Player[]) public winners;
-
-    uint256 private aDay = 100000;
+    mapping(uint256 => Player[]) public winners;
+    
+    uint256 private time_of_a_day = 100000;
     uint256 private beginning_of_the_day;
+    uint256 profits;
 
-    constructor () {
+    event NewPlayer(uint256 id, string pseudo);
+    event WinTheBet(Player winner, uint256 matchId);
+
+    constructor() {
         admin = msg.sender;
-        increment = 0;
+        profits = 0;
         matchs.push(Match({teamA: 1, teamB: 0}));
         matchs.push(Match({teamA: 3, teamB: 1}));
-        matchs.push(Match({teamA: 1, teamB: 1}));
+        matchs.push(Match({teamA: 1, teamB: 0}));
+        matchs.push(Match({teamA: 0, teamB: 1}));
+        matchs.push(Match({teamA: 3, teamB: 1}));
+        matchs.push(Match({teamA: 0, teamB: 1}));
+        matchs.push(Match({teamA: 4, teamB: 3}));
+        matchs.push(Match({teamA: 0, teamB: 1}));
+        matchs.push(Match({teamA: 1, teamB: 0}));
+        matchs.push(Match({teamA: 2, teamB: 5}));
         beginning_of_the_day = block.timestamp;
     }
 
-    function bet(uint256 matchId, uint256 scoreA, uint256 scoreB, string memory your_pseudo) external payable {
+    function bet(
+        uint256 matchId,
+        uint256 scoreA,
+        uint256 scoreB,
+        string memory your_pseudo
+    ) external payable {
         require(matchId < matchs.length, "Bad request");
-        admission(your_pseudo);
+        profits += admission(your_pseudo);
         Match memory match_to_bet = matchs[matchId];
 
         // adding player who won the bet of a Match
-        if (scoreA == match_to_bet.teamA && scoreB == match_to_bet.teamB){
+        if (scoreA == match_to_bet.teamA && scoreB == match_to_bet.teamB) {
             Player memory winner = players[msg.sender];
             emit WinTheBet(winner, matchId);
-            if (isWinner(winner, matchId)){
+            if (isWinner(winner, matchId)) {
                 return;
             }
+            ++winner.victories;
             winners[matchId].push(winner);
         }
     }
 
+    Player[] theWinners;
 
-    function isWinner(Player memory player, uint256 matchId) internal view returns (bool) {
+    function bestWinners() public onlyOwner {
+        require(block.timestamp > beginning_of_the_day + time_of_a_day, "Waiting for the end of the day");
+        for (uint256 i = 0; i < matchs.length; ++i) {
+            Player[] memory winners_of_match = winners[i];
+        }
+        uint256 nb_of_victories = matchs.length;
+        while (theWinners.length < 5 && nb_of_victories > 0) {
+            for (uint256 i = 0; i < participant_number; ++i) {
+                if (players[i].victories == nb_of_victories) {
+                    theWinners.push(players[i]);
+                }
+                if (theWinners.length == 5){
+                    break;
+                }
+            }
+            --nb_of_victories;
+        }
+    }
+
+    function isWinner(
+        Player memory player,
+        uint256 matchId
+    ) internal view returns (bool) {
         for (uint256 i = 0; i < winners[matchId].length; ++i) {
             if (winners[matchId][i].id == player.id) {
                 return true;
@@ -60,14 +99,25 @@ contract SmartBetContract {
         }
         return false;
     }
-    function admission(string memory your_pseudo) internal returns (uint256){
-        require(msg.value == 0.01 ether, "Not the right amount of fees (0.01 ether)");
-        players [msg.sender] = Player ({
-            id: ++increment,
-            pseudo: your_pseudo
+
+    function admission(string memory your_pseudo) internal returns (uint256) {
+        require(
+            msg.value == 0.01 ether,
+            "Not the right amount of fees (0.01 ether)"
+        );
+        players[participant_number] = Player({
+            id: participant_number,
+            pseudo: your_pseudo,
+            player_address: msg.sender,
+            victories: 0
         });
-        emit NewPlayer(increment, your_pseudo);
+        ++participant_number;
+        emit NewPlayer(participant_number, your_pseudo);
         return msg.value;
     }
 
+    modifier onlyOwner() {
+        require(msg.sender == admin, "Not Authorized");
+        _;
+    }
 }
